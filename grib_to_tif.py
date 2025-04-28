525f378a6e2bf5f50a6fd17328552f674422c4dd
Geogueddari <abdeljabbarelgaddari7@gmail.com>
2025-04-28 17:02:14 +0100

    load temperature data from ecmwf plateform as .grib and convert it to .tif

--- grib_to_tif.py
+++ grib_to_tif.py
@@ -1,4 +1,23 @@
 import subprocess
+from ecmwfapi import ECMWFDataServer
+
+server = ECMWFDataServer()
+
+server.retrieve({
+    "class": "s2",
+    "dataset": "s2s",
+    "date": "2025-04-01/to/2025-04-26",
+    "expver": "prod",
+    "levtype": "sfc",
+    "model": "glob",
+    "origin": "ecmf",
+    "param": "121",
+    "step": "6",
+    "stream": "enfo",
+    "time": "00:00:00",
+    "type": "cf",
+    "target": "data.grib"
+})
 
 # Commande gdal_translate à exécuter
 command = ["gdal_translate", "-of", "GTiff", "data.grib", "data.tif"]
